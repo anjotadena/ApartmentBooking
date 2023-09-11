813343718af9aab85dd756853e86d5c5085a424b
Anjo Tadena <tadena.anjo@gmail.com>
2023-09-12 07:58:30 +0800

    Add abstract CQRS

---
+++ ApartmentBooking.Application/Abstractions/Messaging/ICommand.cs
@@ -0,0 +1,17 @@
+﻿using ApartmentBooking.Domain.Abstractions;
+using MediatR;
+
+namespace ApartmentBooking.Application.Abstractions.Messaging;
+
+public interface ICommand : IRequest<Result>, IBaseCommand
+{
+}
+
+public interface ICommand<TResponse> : IRequest<Result<TResponse>>, IBaseCommand
+{
+}
+
+public interface IBaseCommand
+{
+}
+
