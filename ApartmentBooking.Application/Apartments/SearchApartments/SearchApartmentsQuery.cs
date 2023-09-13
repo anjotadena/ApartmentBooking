837343ce8b17b9cd4fc72294344b3128be7fa418
Anjo Tadena <tadena.anjo@gmail.com>
2023-09-13 09:20:18 +0800

    Implement queries with Dapper

---
+++ ApartmentBooking.Application/Apartments/SearchApartments/SearchApartmentsQuery.cs
@@ -0,0 +1,5 @@
+﻿using ApartmentBooking.Application.Abstractions.Messaging;
+
+namespace ApartmentBooking.Application.Apartments.SearchApartments;
+
+public sealed record SearchApartmentsQuery(DateOnly StartDate, DateOnly EndDate) : IQuery<IReadOnlyList<ApartmentResponse>>;
