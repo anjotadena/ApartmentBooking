773b5739afdebf4acd2ad0a35463a7ed8d26d41c
Anjo Tadena <tadena.anjo@gmail.com>
2023-09-14 08:37:58 +0800

    Create domain entity configuration

---
+++ Apartment.Domain/Reviews/Review.cs
@@ -0,0 +1,10 @@
+﻿using ApartmentBooking.Domain.Abstractions;
+
+namespace ApartmentBooking.Domain.Reviews;
+
+public class Review : Entity
+{
+    public Review(Guid id) : base(id)
+    {
+    }
+}
---
+++ ApartmentBooking.Infrastructure/Configurations/ReviewConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using ApartmentBooking.Domain.Reviews;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ApartmentBooking.Infrastructure.Configurations;
+
+public sealed class ReviewConfiguration : IEntityTypeConfiguration<Review>
+{
+    public void Configure(EntityTypeBuilder<Review> builder)
+    {
+        builder.ToTable("reviews");
+    }
+}
