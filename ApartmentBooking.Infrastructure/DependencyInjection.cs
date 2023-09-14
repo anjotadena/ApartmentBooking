4cbb585415d32f9812795cd33ddf48ebd67854d8
Anjo Tadena <tadena.anjo@gmail.com>
2023-09-14 08:08:30 +0800

    Configure infrastructure services with DI

---
+++ ApartmentBooking.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using ApartmentBooking.Application.Abstractions.Clock;
+using ApartmentBooking.Application.Abstractions.Email;
+using ApartmentBooking.Infrastructure.Clock;
+using ApartmentBooking.Infrastructure.Email;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApartmentBooking.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+
+        services.AddTransient<IEmailService, EmailService>();
+
+        return services;
+    }
+}
