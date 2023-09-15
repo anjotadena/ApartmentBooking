68539c03551573b60c17239281b7dddc4f6692bf
Anjo Tadena <tadena.anjo@gmail.com>
2023-09-15 08:22:35 +0800

    Publishing domain events inside the unit of work

--- ApartmentBooking.Infrastructure/ApplicationDbContext.cs
+++ ApartmentBooking.Infrastructure/ApplicationDbContext.cs
@@ -1,12 +1,16 @@
 ﻿using ApartmentBooking.Domain.Abstractions;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace ApartmentBooking.Infrastructure;
 
 public sealed class ApplicationDbContext : DbContext, IUnitOfWork
 {
-    public ApplicationDbContext(DbContextOptions options) : base(options)
+    private readonly IPublisher _publisher;
+
+    public ApplicationDbContext(DbContextOptions options, IPublisher publisher) : base(options)
     {
+        _publisher = publisher;
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -14,5 +18,34 @@ public sealed class ApplicationDbContext : DbContext, IUnitOfWork
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
 
         base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var result = await base.SaveChangesAsync(cancellationToken);
+
+        await PublishDomainEventAsync();
+
+        return result;
+    }
+
+    private async Task PublishDomainEventAsync()
+    {
+        var domainEvents = ChangeTracker.Entries<Entity>()
+                                        .Select(entry => entry.Entity)
+                                        .SelectMany(entity =>
+                                        {
+                                            var domainEvents = entity.GetDomainEvents();
+
+                                            entity.ClearDomainEvents();
+
+                                            return domainEvents;
+                                        })
+                                        .ToList();
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent);
+        }
     }
 }
