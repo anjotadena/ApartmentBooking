987214dd25fe4e6d57d95056c68dbc4542b4837b
Anjo Tadena <tadena.anjo@gmail.com>
2023-09-16 10:29:38 +0800

    Add middleware global exception handler

--- ApartmentBooking.Api/Extensions/ApplicationBuilderExtensions.cs
+++ ApartmentBooking.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using ApartmentBooking.Infrastructure;
+﻿using ApartmentBooking.Api.Middleware;
+using ApartmentBooking.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 namespace ApartmentBooking.Api.Extensions;
@@ -12,5 +13,10 @@ public static class ApplicationBuilderExtensions
         using var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
         dbContext.Database.Migrate();
+    }
+
+    public static void UseCustomExceptionHandler(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
     }
 }
